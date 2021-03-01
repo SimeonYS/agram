2ebddc5171676862a915645d73ac2c09ebb1f315
SimeonYS <simeon.simeonov@ADPVT.com>
2021-03-01 15:54:22 +0200

    Done

---
+++ README.md
@@ -0,0 +1,9 @@
+URL: https://www.agrambanka.hr/novosti-2/
+
+Spider name: agram
+DB Schema:
+
+date
+title
+link
+content
---
+++ agram/items.py
@@ -0,0 +1,8 @@
+import scrapy
+
+
+class AgramItem(scrapy.Item):
+    title = scrapy.Field()
+    content = scrapy.Field()
+    date = scrapy.Field()
+    link = scrapy.Field()
